596966a16d65d7d7f06846dbb40216b6245b7d56
virendra katariya <virendra.katariya1710@gmail.com>
2022-07-25 12:31:54 +0530

    lead view member wise category subCategory Product

--- src/routes/lead/index.js
+++ src/routes/lead/index.js
@@ -37,7 +37,7 @@ router.delete('/delete/:leadId', isLoggedIn, catchAsync(async (req, res) => {
 
 
 router.get('/:memberId', isLoggedIn, catchAsync(async (req, res) => {
-    
+
     const id = parseInt(req.params.memberId);
 
     if (!id)
@@ -51,8 +51,8 @@ router.get('/:memberId', isLoggedIn, catchAsync(async (req, res) => {
             memberId: id
         },
         include: {
-            SubCategory : {
-                select : {
+            SubCategory: {
+                select: {
                     Category: true,
                     name: true,
                     id: true
@@ -62,10 +62,10 @@ router.get('/:memberId', isLoggedIn, catchAsync(async (req, res) => {
     })
 
     if (lead.length == 0) return res.status(404).json({ msg: `Lead not found`, data: [] });
-    
+
     let new_array = [];
-    for(let i = 0 ; i < lead.length ; i ++) {
-        if(i == 0 ) {
+    for (let i = 0; i < lead.length; i++) {
+        if (i == 0) {
             new_array.push({
                 id: lead[i].SubCategory.Category.id,
                 name: lead[i].SubCategory.Category.name,
@@ -77,8 +77,8 @@ router.get('/:memberId', isLoggedIn, catchAsync(async (req, res) => {
             })
         } else {
             let check = 0;
-            for(let j = 0 ; j < new_array.length ; j ++) {
-                if(new_array[j].id == lead[i].SubCategory.Category.id) {
+            for (let j = 0; j < new_array.length; j++) {
+                if (new_array[j].id == lead[i].SubCategory.Category.id) {
                     new_array[j].subCategory.push({
                         id: lead[i].SubCategory.id,
                         name: lead[i].SubCategory.name,
@@ -87,7 +87,7 @@ router.get('/:memberId', isLoggedIn, catchAsync(async (req, res) => {
                     check = 1;
                 }
             }
-            if(check == 0) { 
+            if (check == 0) {
                 new_array.push({
                     id: lead[i].SubCategory.Category.id,
                     name: lead[i].SubCategory.Category.name,
@@ -100,26 +100,97 @@ router.get('/:memberId', isLoggedIn, catchAsync(async (req, res) => {
             }
         }
     }
-    for(let i = 0 ; i < new_array.length ; i ++) {
+    for (let i = 0; i < new_array.length; i++) {
         for (let j = 0; j < new_array[i].subCategory.length; j++) {
             const lead_member = await prisma.need.findMany({
                 where: {
-                    AND:[
-                        {categoryId: new_array[i].id},
-                        {subCategoryId: new_array[i].subCategory[j].id},
-                        {status: "Active"}
+                    AND: [
+                        { categoryId: new_array[i].id },
+                        { subCategoryId: new_array[i].subCategory[j].id },
+                        { status: "Active" }
                     ],
                     NOT: [
-                        {memberId: id}
+                        { memberId: id }
                     ]
                 },
-                select:{
+                select: {
                     Member: true
                 }
-            }) 
+            })
             let lead = []
-            for(let k = 0 ; k < lead_member.length ; k ++) {
-                // console.log(lead_member[]);
+            for (let k = 0; k < lead_member.length; k++) {
+                // * for member according category -> subCategory -> products start 
+                const data = await prisma.memberSubCategory.findMany({
+                    where: {
+                        memberId: lead_member[k].Member.id
+                    },
+                    select: {
+                        SubCategory: {
+                            select: {
+                                id: true,
+                                name: true,
+                                Category: true
+                            }
+                        }
+                    }
+                })
+
+                let memberCategory = [];
+                for (let v = 0; v < data.length; v++) {
+
+                    
+                    const pro = await prisma.my_product.findMany({
+                        where: {
+                            AND: [
+                                { memberId: lead_member[k].Member.id },
+                                {Product: {
+                                    categoryId: data[v].SubCategory.Category.id,
+                                    subCategoryId: data[v].SubCategory.id
+                                }}
+                            ]
+                        },
+                        select:{
+                            Product: true
+                        }
+                    })
+                    
+                    if (v == 0) {
+                        memberCategory.push({
+                            id: data[v].SubCategory.Category.id,
+                            name: data[v].SubCategory.Category.name,
+                            subCategory: [{
+                                id: data[v].SubCategory.id,
+                                name: data[v].SubCategory.name,
+                                product: pro
+                            }]
+                        })
+                    } else {
+                        let check = 0;
+                        for (let d = 0; d < memberCategory.length; d++) {
+                            if (memberCategory[d].id == data[v].SubCategory.Category.id) {
+                                memberCategory[d].subCategory.push({
+                                    id: data[v].SubCategory.id,
+                                    name: data[v].SubCategory.name,
+                                    product: pro
+                                })
+                                check = 1;
+                            }
+                        }
+                        if (check == 0) {
+                            memberCategory.push({
+                                id: data[v].SubCategory.Category.id,
+                                name: data[v].SubCategory.Category.name,
+                                subCategory: [{
+                                    id: data[v].SubCategory.id,
+                                    name: data[v].SubCategory.name,
+                                    product: pro
+                                }]
+                            })
+                        }
+                    }
+                }
+                lead_member[k].Member.category = memberCategory;
+                // * for member according category -> subCategory -> products start
                 lead.push(lead_member[k].Member)
             }
             new_array[i].subCategory[j].member = lead;
